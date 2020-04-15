1a4060748a66833f2a8ad90298eb5b3f78a3958f
DarkTechLC <cliciosa14@gmail.com>
2020-04-15 10:04:43 -0300

    Add new features

--- lab/decbin/js/script.js
+++ lab/decbin/js/script.js
@@ -2,6 +2,8 @@ const inputBin = document.querySelector('#bin-input');
 const inputDec = document.querySelector('#dec-input');
 const btnConvert = document.querySelector('#btn-convert');
 
+let binToDecOrDecToBin;
+
 function binaryToDecimal(binaryNumber) {
   let decimalNumber = 0;
 
@@ -34,30 +36,60 @@ function alert(element) {
   element.focus();
 }
 
+function checkInputBinDataAndConvertToDecimal() {
+  const regex = /[^0-1]/g;
+  let testValue = regex.exec(inputBin.value);
+
+  if (testValue) {
+    alert(inputBin);
+  } else {
+    let decimal = binaryToDecimal(inputBin.value);
+    inputDec.value = decimal;
+  }
+}
+
+function checkInputDecDataAndConvertToBinary() {
+  const regex = /[^0-9]/g;
+  let testValue = regex.exec(inputDec.value);
+
+  if (testValue) {
+    alert(inputDec);
+  } else {
+    let binary = decimalToBinary(inputDec.value);
+    inputBin.value = binary;
+  }
+}
+
 function convertValue() {
   if (inputBin.value !== '') {
-    const regex = /[^0-1]/g;
-    let testValue = regex.exec(inputBin.value);
-
-    if (testValue) {
-      alert(inputBin);
-    } else {
-      let decimal = binaryToDecimal(inputBin.value);
-      inputDec.value = decimal;
-    }
+    checkInputBinDataAndConvertToDecimal();
   } else if (inputDec.value !== '') {
-    const regex = /[^0-9]/g;
-    let testValue = regex.exec(inputDec.value);
-
-    if (testValue) {
-      alert(inputDec);
-    } else {
-      let binary = decimalToBinary(inputDec.value);
-      inputBin.value = binary;
-    }
+    checkInputDecDataAndConvertToBinary();
   }
 }
 
 inputBin.addEventListener('focus', () => inputDec.value = '');
 inputDec.addEventListener('focus', () => inputBin.value = '');
 btnConvert.addEventListener('click', convertValue);
+
+document.body.addEventListener('keypress', e => {
+  if (e.key === 'Enter') {
+    if (binToDecOrDecToBin) {
+      if (binToDecOrDecToBin === 'binToDec') {
+        checkInputBinDataAndConvertToDecimal();
+      } else if (binToDecOrDecToBin === 'decToBin') {
+        checkInputDecDataAndConvertToBinary();
+      }
+    } else {
+      if (inputBin.value !== '') {
+        binToDecOrDecToBin = 'binToDec';
+        checkInputBinDataAndConvertToDecimal();
+      } else if (inputDec.value !== '') {
+        binToDecOrDecToBin = 'decToBin';
+        checkInputDecDataAndConvertToBinary();
+      } else {
+        binToDecOrDecToBin = '';
+      }
+    }
+  }
+});
